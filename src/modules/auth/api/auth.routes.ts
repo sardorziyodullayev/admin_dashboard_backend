814373461cb57547a2fd4ad91f7f75bcd121bdc0
Sardor <106055019+sardorziyodullayev@users.noreply.github.com>
2025-12-30 00:12:08 +0500

    feat(auth): add authentication middleware and update routes for user login and profile access

--- src/modules/auth/api/auth.routes.ts
+++ src/modules/auth/api/auth.routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { registerController } from "./auth.controller";
+import { loginController, registerController } from "./auth.controller";
+import { authMiddleware } from "../../../shared/middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/register", registerController);
+router.post("/login", loginController);
+
+router.get("/me", authMiddleware, (req: any, res) => {
+   res.json({
+      message: "Protected route",
+      user: req.user,
+   });
+});
 
 export default router;
---
+++ src/shared/middleware/auth.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export interface AuthRequest extends Request {
+   user?: {
+      userId: string;
+      role: string;
+   };
+}
+
+export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+   try {
+      const authHeader = req.headers.authorization;
+
+      if(!authHeader) {
+         return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+      const token = authHeader.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+      req.user = decoded as any;
+      next();
+   } catch (error) {
+      return res.status(401).json({ message: 'Unauthorized' });
+   }
+}
