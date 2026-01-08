1f0e15586beff5f5c47adca1a6aa3e84ddc80d82
Sardor <106055019+sardorziyodullayev@users.noreply.github.com>
2026-01-08 22:36:47 +0500

    feat(auth): enhance refresh token functionality to return new refresh token and access token

--- src/modules/auth/api/refresh.controller.ts
+++ src/modules/auth/api/refresh.controller.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express";
 import { RefreshTokenUseCase } from "../application/refresh-token.usecase";
+import { RefreshTokenRepository } from "../infrastructure/refresh-token.repository";
 
-export const refreshTokenController = (req: Request, res: Response) => {
-   const { refreshToken } = req.body;
+export const refreshTokenController = async (req: Request, res: Response) => {
+   try {
+      const { refreshToken } = req.body;
 
-   if (!refreshToken) {
-      return res.status(400).json({ message: 'Refresh token is required' });
+      if (!refreshToken) {
+         return res.status(400).json({ message: 'Refresh token is required' });
+      }
+
+      const useCase = new RefreshTokenUseCase(
+         new RefreshTokenRepository()
+      );
+
+      const result = await useCase.execute(refreshToken);
+
+      return res.status(200).json(result);
+   } catch (error: any) {
+      return res.status(401).json({ message: error.message });
    }
-
-   const useCase = new RefreshTokenUseCase();
-   const result = useCase.execute(refreshToken);
-
-   return res.status(200).json(result);
-}
+};
--- src/modules/auth/application/refresh-token.usecase.ts
+++ src/modules/auth/application/refresh-token.usecase.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { env } from "../../../shared/config/env"
-import { generateAccessToken } from '../../../shared/utils/token';
+import { generateAccessToken, generateRefreshToken } from '../../../shared/utils/token';
 import { RefreshTokenRepository } from '../infrastructure/refresh-token.repository';
 
 interface RefreshTokenPayload {
@@ -11,29 +11,47 @@ interface RefreshTokenPayload {
 export class RefreshTokenUseCase {
    constructor(private refreshTokenRepo: RefreshTokenRepository) { }
 
-   async execute(refreshToken: string): Promise<{ accessToken: string }> {
+   async execute(oldRefreshToken: string): Promise<{ accessToken: string, refreshToken: string }> {
       let decoded: RefreshTokenPayload;
 
       try {
          decoded = jwt.verify(
-            refreshToken,
+            oldRefreshToken,
             env.JWT_REFRESH_SECRET
          ) as RefreshTokenPayload;
       } catch {
          throw new Error("Invalid refresh token");
       }
 
-      const storedToken = await this.refreshTokenRepo.findValidToken(refreshToken);
+      const storedToken = await this.refreshTokenRepo.findValidToken(oldRefreshToken);
 
       if (!storedToken) {
          throw new Error("Refresh token expired or not found");
       }
+
+      await this.refreshTokenRepo.deleteByToken(oldRefreshToken);
+
+      const newRefreshToken = generateRefreshToken({
+         userId: decoded.userId,
+         role: decoded.role,
+      })
 
       const accessToken = generateAccessToken({
          userId: decoded.userId,
          role: decoded.role,
       });
 
-      return { accessToken };
+      await this.refreshTokenRepo.save({
+         userId: decoded.userId,
+         token: newRefreshToken,
+         expiresAt: new Date(
+            Date.now() + 7 * 24 * 60 * 60 * 1000 
+         ),
+      })
+
+      return {
+         accessToken,
+         refreshToken: newRefreshToken,
+      };
    }
 }
